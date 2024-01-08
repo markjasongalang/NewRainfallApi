f791cb43a046513f9d45fd432378436fab859dbf
Jason <Jason@DESKTOP-E1RKIMD>
2024-01-08 23:58:53 +0800

    Add project files.

---
+++ NewRainfallApi/Controllers/RainfallController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewRainfallApi.Controllers
+{
+    [Route("api/rainfall")]
+    [ApiController]
+    public class RainfallController : ControllerBase
+    {
+        [HttpGet("id/{stationId}/readings")]
+        public async Task<IActionResult> GetRainfallReadings(string stationId, int count = 10)
+        {
+
+            return Ok();
+        }
+    }
+}
